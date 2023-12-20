f9a6ee8422301abae1a447b1cd641e2c2fe27394
Stefan Zaharie <stefan@zaha.tech>
2023-12-20 19:18:13 +0200

    Minor doc change

--- daemon/include/daemonize.h
+++ daemon/include/daemonize.h
@@ -2,7 +2,7 @@
 #define DAEMON_DAEMONIZE_H
 
 /*
- * Make the process a daemon using double fork. See TIP chapter 37.
+ * Make the process a daemon using double fork. See daemon(7).
  * Returns 0 on success, 1 on failure.
  */
 unsigned short daemonize(void);
