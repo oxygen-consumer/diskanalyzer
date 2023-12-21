26ab2b0d2aacdb4f906a762c13a9732478b75020
Stefan Zaharie <stefan@zaha.tech>
2023-12-21 15:56:55 +0200

    Document die() function

--- daemon/include/utils.h
+++ daemon/include/utils.h
@@ -3,6 +3,11 @@
 
 #include <stdbool.h>
 
+/*
+ * Kill the daemon and print the message to syslog.
+ * If ok is false, the message will be printed as an error,
+ * or info otherwise.
+ */
 void die(bool ok, const char *msg, ...);
 
 #endif
