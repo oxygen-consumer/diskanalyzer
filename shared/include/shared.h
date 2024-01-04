4c020b209ae7ec643d4f2530f0256bd16fad9a1c
Popa Sebastian <sebastianpopa208@gmail.com>
2024-01-04 21:26:32 +0200

    Add enum and struct for response

--- shared/include/shared.h
+++ shared/include/shared.h
@@ -38,4 +38,21 @@ struct message
     enum Priority priority;
 };
 
+enum ResponseCode
+{
+    OK,
+    INVALID_ID_ERROR,             // there is not task with such id
+    TASK_ALREADY_FINISHED_ERROR,  // if task is finished, it can't be suspended/resumed
+    TASK_ALREADY_SUSPENDED_ERROR, // cannot suspend task that is already suspended
+    TASK_ALREADY_RUNNING_ERROR,   // cannot resume task that is already running
+    TASK_NOT_DONE_ERROR,          // cannot print info about task that is not done
+    GENERAL_ERROR,                // really bad things happened
+};
+
+struct response
+{
+    enum ResponseCode response_code;
+    char path[MAX_PATH_SIZE];
+};
+
 #endif
